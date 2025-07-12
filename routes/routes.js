1248746d0b047c01c7b6d14888bd530226695f7b
Crisber <crisberjosepambichoreynoso@gmail.com>
2025-07-12 12:59:45 -0500

    Implementar tarjeta uniforme en sección de testimonios

--- routes/routes.js
+++ routes/routes.js
@@ -2,14 +2,10 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.render('index'); // ¡sin extensión .ejs!
-});
+router.get('/', (req, res) => {res.render('index'); });
 
-router.get('/servicios', (req, res) => {
-  res.render('servicios');
-});
+router.get('/servicios', (req, res) => {res.render('servicios');});
 
-
+router.get('/quienes-somos', (req, res) => {res.render('quienes-somos');});
 module.exports = router;
 
