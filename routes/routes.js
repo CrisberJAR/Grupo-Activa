7a618ceab42049469f557e2437be7137a34ccb3b
Crisber <cris.ambicho@gmail.com>
2025-07-15 11:12:56 -0500

    Mejorar simulador: tarjetas una debajo de otra, colores de iconos y limpieza de badges

--- routes/routes.js
+++ routes/routes.js
@@ -7,5 +7,7 @@ router.get('/', (req, res) => {res.render('index'); });
 router.get('/servicios', (req, res) => {res.render('servicios');});
 
 router.get('/quienes-somos', (req, res) => {res.render('quienes-somos');});
+
+router.get('/simulador', (req, res) => {res.render('simulador');});
 module.exports = router;
 
